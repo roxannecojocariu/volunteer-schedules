e517e01d3800a5cbfa1328fb81dc5d504a05ddb1
Roxanne Cojocariu <rmcojocariu@gmail.com>
2019-03-04 16:37:30 -0500

    fix sign out error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
