4ba043078f860b77368e29e5f764afbed5de0049
Roxanne Cojocariu <rmcojocariu@gmail.com>
2019-03-04 16:36:26 -0500

    add additional user column

---
+++ db/migrate/20190304212802_add_additional_user_columns.rb
@@ -0,0 +1,8 @@
+class AddAdditionalUserColumns < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+    add_column :users, :address, :string
+    add_column :users, :phone_number, :string
+  end
+end
